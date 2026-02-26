a88ae7f2c78387176ca351c7038b96f1e3916941
LSB7 <lengsambthsb2004@gmail.com>
2026-02-26 20:07:22 +0700

    remove header

--- lib/api.ts
+++ lib/api.ts
@@ -6,8 +6,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://renata-probeabl
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
-        'Content-Type': 'application/json',
-        'ngrok-skip-browser-warning': 'true',
+        'Content-Type': 'application/json'
     },
     timeout: 15000,
 });
